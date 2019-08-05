0b9f4d6468b6ca823b1f8e212d07c6db367443f7
deeksha <bharathraj.deeksha@gmail.com>
2019-08-05 19:39:40 +0530

    adding files

---
+++ grade2/app.py
@@ -0,0 +1,49 @@
+import flask
+
+import pandas as pd
+
+
+from sklearn.externals import joblib
+
+regressor = joblib.load('model/model.pkl')
+
+
+app = flask.Flask(__name__, template_folder='templates')
+
+@app.route('/grade2', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        return(flask.render_template('main.html'))
+    
+    if flask.request.method == 'POST':
+        
+        sex = flask.request.form['sex']
+        age = flask.request.form['age']
+        studytime = flask.request.form['studytime']
+        failures = flask.request.form['failures']
+        freetime = flask.request.form['freetime']
+        absences = flask.request.form['absences']
+        goout = flask.request.form['goout']
+        first = flask.request.form['first']
+        second = flask.request.form['second']
+        
+        
+
+
+        input_variables = pd.DataFrame([[sex,age,studytime,failures,freetime,absences,goout,first,second]],
+                                       columns=['sex', 'age', 'studytime','failures','freetime','absences','goout','first','second'],
+                                       dtype=float,
+                                       index=['input'])
+
+        prediction = regressor.predict(input_variables)[0]
+        
+    
+        return flask.render_template('main1.html',
+                                   
+                        
+                                     result=prediction
+                                     )
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ grade2/grade2.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('grades.csv')
+
+x=dataset.iloc[:, :-1].values
+y=dataset.iloc[:, 9].values
+
+from sklearn.preprocessing import LabelEncoder
+labelencoder=LabelEncoder()
+x[:,0]=labelencoder.fit_transform(x[:,0])
+
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(x[:, :-1])
+x[:,:-1 ] = imputer.transform(x[:, :-1])
+
+
+from sklearn.cross_validation import train_test_split
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
+
+from sklearn.ensemble import ExtraTreesClassifier
+model1 = ExtraTreesClassifier()
+model1.fit(x,y)
+feat_importances = pd.Series(model1.feature_importances_)
+feat_importances.nlargest(10).plot(kind='barh')
+plt.show()
+
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+x=sc.fit_transform(x)
+
+
+from sklearn.tree import DecisionTreeRegressor
+regressor=DecisionTreeRegressor()
+regressor.fit(x_train,y_train)
+
+y_pred=regressor.predict(x_test)
+errors = abs(y_pred - y_test)
+print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
+
+from sklearn.externals import joblib
+joblib.dump(regressor,'model/model.pkl')
